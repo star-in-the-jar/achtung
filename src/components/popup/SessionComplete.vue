6ac9bceafcc9485687a0e60363cb02dcd43c47ba
Władysław Kuczerenko <w.kuczerenko@crehler.com>
2024-04-06 17:14:17 +0200

    test

---
+++ src/components/popup/SessionComplete.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="bg-green-500 text-slate-900 h-screen w-screen flex items-center justify-center flex-col"
+  >
+    <div class="flex gap-x-[13px] items-center mb-5">
+      <TriangleAlert size="44" class="stroke-1" />
+      <h2 class="text-3xl font-semibold">You're losing focus!</h2>
+    </div>
+    <div class="flex items-center gap-x-4">
+      <Button>TAKE A BREAK</Button>
+      <Button variant="outline">SKIP</Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { TriangleAlert } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+</script>
