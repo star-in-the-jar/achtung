fe4d82169ad68bbfdf697f5f2bdc55e947b91c40
szymonjozwiakzip <jozwiakszymon03@gmail.com>
2024-04-06 18:01:59 +0200

    Timer and menu in main page added (not completed)

--- src/components/ui/select/index.ts
+++ src/components/ui/select/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export { default as Select } from "./Select.vue";
 export { default as SelectValue } from "./SelectValue.vue";
 export { default as SelectTrigger } from "./SelectTrigger.vue";
@@ -10,3 +11,16 @@ export { default as SelectSeparator } from "./SelectSeparator.vue";
 export { default as SelectScrollUpButton } from "./SelectScrollUpButton.vue";
 export { default as SelectScrollDownButton } from "./SelectScrollDownButton.vue";
 export { default as ReadySelect } from "./ReadySelect.vue";
+=======
+export { default as Select } from './Select.vue'
+export { default as SelectValue } from './SelectValue.vue'
+export { default as SelectTrigger } from './SelectTrigger.vue'
+export { default as SelectContent } from './SelectContent.vue'
+export { default as SelectGroup } from './SelectGroup.vue'
+export { default as SelectItem } from './SelectItem.vue'
+export { default as SelectItemText } from './SelectItemText.vue'
+export { default as SelectLabel } from './SelectLabel.vue'
+export { default as SelectSeparator } from './SelectSeparator.vue'
+export { default as SelectScrollUpButton } from './SelectScrollUpButton.vue'
+export { default as SelectScrollDownButton } from './SelectScrollDownButton.vue'
+>>>>>>> aaabf82 (Timer and menu in main page added (not completed))
